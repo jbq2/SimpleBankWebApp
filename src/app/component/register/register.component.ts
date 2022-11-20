bdac98912b8da9ffeafcb7e286911c883a00af94
Joshua Quizon <joshquizon@gmail.com>
2022-11-20 17:41:32 -0500

    valid variable is in the class level--makes it visible to html

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -15,6 +15,7 @@ export class RegisterComponent implements OnInit {
     password: '',
     matching: ''
   };
+  valid: boolean = true;
 
   /* registration service will be used to send req to api */
   constructor(private registrationService: RegistrationService) { }
@@ -29,51 +30,50 @@ export class RegisterComponent implements OnInit {
     console.log(this.registrationData.matching);
     */
 
-    let valid: boolean = true;
     let emailRegex: string = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$';
     let passwordRegex: string = '^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$';
     
     /* TODO: if invalid, post flash message saying was exactly went wrong */
     let regex: RegExp = new RegExp(emailRegex);
     if(this.registrationData.email.length == 0){
-      valid = false;
+      this.valid = false;
       console.warn('Email field must not be empty');
     }
     else{
       if(!regex.test(this.registrationData.email)){
-        valid = false;
+        this.valid = false;
         console.warn('Entered email is invalid');
       }
     }
     
     regex = new RegExp(passwordRegex);
     if(this.registrationData.password.length == 0){
-      valid = false;
+      this.valid = false;
       console.warn('Password field must not be empty');
     }
     else{
       if(this.registrationData.password.length < 8){
-        valid = false;
+        this.valid = false;
         console.warn('Entered password is too short (must have >= 8 characters)');
       }
       if(!regex.test(this.registrationData.password)){
-        valid = false;
+        this.valid = false;
         console.warn('Entered password is invalid (atleast 1 letter, 1 special character, 1 digit)');
       }
     }
 
     if(this.registrationData.matching.length == 0){
-      valid = false;
+      this.valid = false;
       console.warn('Password confirmation field must not be empty');
     }
     else{
       if(this.registrationData.password != this.registrationData.matching){
-        valid = false;
+        this.valid = false;
         console.warn('Entered password confirmation does not match entered password');
       }
     }
 
-    if(valid){
+    if(this.valid){
       /* TODO: if valid, proceed send registrationData to api */
       console.warn('Valid form submission');
     }
