fc6f88dd56adaaa5ff1bdf1e2dc4e424f27576c7
Joshua Quizon <joshquizon@gmail.com>
2022-11-20 12:20:12 -0500

    adding angular form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +15,10 @@ import { RegisterComponent } from './component/register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,3 +1,6 @@
+import { RegistrationService } from './../../service/registration.service';
+import { FormBuilder } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm = this.formBuilder.group({
+    email: '',
+    password: '',
+    matching: ''
+  }); 
 
-  ngOnInit(): void {
+  // private registrationService: RegistrationService, private formBuilder: FormBuilder
+  constructor(private registrationService: RegistrationService, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void { }
+
+  onSubmit(): void {
+    console.warn('Your registration information has been submitted');
+    /* this will have more stuff in it, like calling the api */
   }
 
 }
