5dc9cb9fdf088980d816946833e089dbe1027a84
Joshua Quizon <joshquizon@gmail.com>
2022-12-03 16:30:30 -0500

    dynamic (untested) and responsive navbar

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { LoginService } from './../../service/login.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,14 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  tabs: Array<string> = []
   baseUrl: string = 'http://localhost:4200';
+  tabs = new Map([
+    ['Register', `${this.baseUrl}/register`],
+    ['Login', `${this.baseUrl}/login`]
+  ]);
 
-  constructor() { 
+  constructor(private loginService: LoginService) { }
 
+  ngOnInit(): void { 
+    let sessionId = localStorage.getItem('SESSION_ID');
+    if(this.loginService.isLoggedIn(sessionId)) {
+      // it is implied that localStorage will have AUTHORITIES if isLoggedIn
+      let authorities: Array<string> = JSON.parse(localStorage.getItem('AUTHORITIES')!);
+      if(authorities.includes('ADMIN')){
+        this.tabs = new Map([
+          ['Accounts', `#`],
+          ['Profile', `#`],
+          ['Admin', `#`],
+          ['Logout', `#`]
+        ]);
+      }
+      else if(authorities.includes('USER')){
+        this.tabs = new Map([
+          ['Accounts', `#`],
+          ['Profile', `#`],
+          ['Logout', `#`]
+        ]);
+      }
+    }
   }
-
-  ngOnInit(): void {
-  }
-
 }
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,7 +1,7 @@
 import { ApiLink } from './../constant/api-link';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Login } from './../interface/login';
-import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Functions } from '../lib/functions';
 import { LoginResponse } from '../interface/login-response';
@@ -17,5 +17,24 @@ export class LoginService {
     /* returns CustomResponse wrapped in Observable; to be processed in the login.component.ts */
     return this.http.post<LoginResponse>(`${ApiLink.local}/login`, loginData)
     .pipe(catchError(Functions.handleHttpError));
+  }
+
+  isLoggedIn(sessionId: string | null): boolean{
+    let result = false;
+    if(sessionId == null){
+      return result;
+    }
+    
+    const headers = new HttpHeaders()
+    .set('Authorization', sessionId);
+    this.http.get<any>(`${ApiLink.local}/verify`, {'headers': headers}).subscribe({
+      next: () => {
+        result = true;
+      },
+      error: () => {
+        result = false
+      }
+    });
+    return result;
   }
 }
