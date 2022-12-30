2603d5285e5b72d45111d404793618570d63b65e
jbq2 <jbq2@njit.edu>
2022-12-30 13:59:47 -0500

    committing errors, gonna branch out to make changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { JwtModule } from 'angular-jwt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/service/update-profile.service.ts
+++ src/app/service/update-profile.service.ts
@@ -1,14 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { ApiLink } from '../constant/api-link';
+import { Update } from '../interface/update';
+import { UpdateProfileResponse } from '../interface/update-profile-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateProfileService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
 
-  requestToUpdate() {
-
+  requestToUpdate(updateData: Update) {
+    let authsJson = localStorage.getItem('AUTHORITIES');
+    let auths = (authsJson == null) ? [] : JSON.parse(authsJson);
+    const claims = {
+      email: localStorage.getItem('EMAIL'),
+      authorities: auths
+    };
+    const jwt = this.jwtHelper.;
+    this.http.post<UpdateProfileResponse>(`${ApiLink.local}/user/update`, updateData);
   }
 }
